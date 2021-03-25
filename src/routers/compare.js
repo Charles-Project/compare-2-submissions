a790ad3cfb669beeead56a9bb966580752078f18
ErnestMarc <ernestpberry@gmail.com>
2021-03-25 14:06:39 +0100

    updated the route

--- src/routers/compare.js
+++ src/routers/compare.js
@@ -6,6 +6,7 @@ const router = new express.Router();
 var fs = require('fs');
 
 const stringSimilarity = require('string-similarity');
+const Diff = require('diff');
 const upload = require("../utils/multer");
 
 router.post("/compare", auth, upload.fields([
@@ -34,12 +35,13 @@ try {
     })
 
     const compareData = await compare.save();
-    res.send({student1,  student2, compareData})
+    const result = `The percentage of similarity is ${compareData.percentage}%`
+    res.send({result, message: {msgBody: `The percentage of similarity is ${compareData.percentage}%`, msgError: false }})
 
   } catch (e) {
     res
       .status(400)
-      .send({ message: { msgBody: "Information not Created", msgError: true }, e });
+      .send({ message: { msgBody: "System Failed", msgError: true }, e });
   }
 });
 
@@ -47,7 +49,52 @@ router.get("/history", auth,  async (req, res) => {
 
 try {
     const compareData = await Compare.find();
-    res.send({compareData})
+    let comaprisonData = [];
+    compareData.forEach(value=>{
+        comaprisonData.push({
+            txt: `The comparism is between ${value.Student1.name} and ${value.Student2.name}  and the similarity is ${value.percentage}`,
+            id: value._id
+        })
+    })
+    res.send({comaprisonData})
+
+  } catch (e) {
+    res
+      .status(400)
+      .send({ message: { msgBody: "History not fetched", msgError: true }, e});
+  }
+});
+
+router.post("/compare/:id", auth,  async (req, res) => {
+
+try {
+    const compareData = await Compare.find({
+        _id: req.params.id
+    });
+
+   let result = `The comparism is between ${compareData[0].Student1.name} and ${compareData[0].Student2.name}  and the similarity is ${compareData[0].percentage}`
+    res.send({Result: result})
+
+  } catch (e) {
+    res
+      .status(400)
+      .send({ message: { msgBody: "Did not fetched the result of the comparism", msgError: true }, e });
+  }
+});
+
+router.post("/compare/run/:id", auth, async (req, res) => {
+try {
+     const compareData = await Compare.find({
+        _id: req.params.id
+    });
+    console.log("value1");
+    const {Student1, Student2} = compareData[0];;
+    const similarity = stringSimilarity.compareTwoStrings(Student1.txt, Student2.txt);
+    const percentage = Math.round(similarity*100);
+
+   let result = `The comparism is between ${Student1.name} and ${Student2.name}  and the similarity is ${percentage}`
+
+    res.send({result})
 
   } catch (e) {
     res
@@ -55,7 +102,6 @@ try {
       .send({ message: { msgBody: "Information not Created", msgError: true }, e });
   }
 });
-
 
 
 module.exports = router;
